e1beafe8ed76af209ee5490abc20889e7f052b7a
thanhnguyencfa <thanhnguyencfa@gmail.com>
2022-12-04 16:46:01 +0700

    Added my project

---
+++ app/src/main/java/com/example/basic101/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.basic101;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.fragment.app.FragmentActivity;
+
+
+
+
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ArrayList<String> rssLinks = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnRediff = findViewById(R.id.btnRediff);
+        Button btnCinemaBlend = findViewById(R.id.btnCinemaBlend);
+        btnRediff.setOnClickListener(this);
+        btnCinemaBlend.setOnClickListener(this);
+        rssLinks.add("http://www.rediff.com/rss/moviesreviewsrss.xml");
+        rssLinks.add("http://www.cinemablend.com/rss_review.php");
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), RSSFeedActivity.class);
+        /*
+        switch (view.getId()) {
+            case R.id.btnRediff:
+                intent.putExtra("rssLink", rssLinks.get(0));
+                startActivity(intent);
+                break;
+            case R.id.btnCinemaBlend:
+                intent.putExtra("rssLink", rssLinks.get(1));
+                startActivity(intent);
+                break;
+
+         */
+
+        if(view.getId()==R.id.btnRediff){
+            intent.putExtra("rssLink", rssLinks.get(0));
+            startActivity(intent);
+        }
+        else
+        if(view.getId()==R.id.btnCinemaBlend) {
+            intent.putExtra("rssLink", rssLinks.get(1));
+            startActivity(intent);
+        }
+
+
+    }
+}
+
---
+++ app/src/main/java/com/example/basic101/RSSItem.java
@@ -0,0 +1,26 @@
+package com.example.basic101;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class RSSItem {
+    public String title;
+    public String link;
+    public String description;
+    public String pubdate;
+    public String guid;
+
+    public RSSItem(String title, String link, String description, String pubdate, String guid) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.pubdate = pubdate;
+        this.guid = guid;
+    }
+}
